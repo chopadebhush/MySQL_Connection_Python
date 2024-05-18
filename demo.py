baf3ae359b0765ab7cc9bcd9f3d7320e1b34e697
chopadebhush <chopadebhush@gmail.com>
2024-05-18 09:09:55 +0530

    Mysql Python V1

--- demo.py
+++ demo.py
@@ -1,13 +1,13 @@
 import mysql.connector
 # import mysql.connector
 #create user 'user'@'%' identified by 'password'
-mydb = mysql.connector.connect(
+con = mysql.connector.connect(
   host="localhost",
   user="abc",
   password="password"
 )
-print(mydb)
-mycursor = mydb.cursor()
-mycursor.execute("SHOW DATABASES")
-for x in mycursor:
-  print(x)
+cursor =con.cursor()
+cursor.execute(")
+for i in cursor.fetchall():
+  print(i)
+
